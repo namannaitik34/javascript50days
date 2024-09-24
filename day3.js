e584c15299ff397bd7e0fee5983440ca07c47b08
Naitik Shrivastawa <128304542+namannaitik34@users.noreply.github.com>
2024-09-24 23:35:14 +0530

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,12 @@
+const accountId = 144553 
+// const can not be changed. it is immutable
+let accountEmail = "namannaitik@google.com"
+var accounctPassword = "Naitik@2024"
+
+// prefer not to use var because of issue in block scope and functional scope
+// use let variables
+
+accountCity = "Jaipur"
+
+console.table([accountId,accountEmail,accounctPassword,accountCity])
+
